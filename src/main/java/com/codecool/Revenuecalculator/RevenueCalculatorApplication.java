573a7701f94c4f7f9e6615c90b48abe3062119bb
Poga <pozsonyigabor96@gmail.com>
2023-06-13 18:53:15 +0200

    Add .gitignore with base files

---
+++ src/main/java/com/codecool/Revenuecalculator/RevenueCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.Revenuecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenueCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevenueCalculatorApplication.class, args);
+	}
+
+}
